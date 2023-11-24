ccb797e4cc9e00926fc515ec5797fbf7d777959a
Читава Александр Валикоевич <chitava@lsin.ln>
2023-11-24 11:28:32 +0300

    Первое задание JavaJunior

--- HW1/untitled/src/main/java/org/example/Main.java
+++ HW1/untitled/src/main/java/org/example/Main.java
@@ -16,15 +16,16 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
-        int[] numbers = IntStream.range(0,100).map(x ->{
+        int[] numbers = IntStream.range(0,10).map(x ->{
+            x = rand.nextInt();
             if (x < 0){
                 x = x * -1;
-            }else rand.nextInt();
-            return x;
-        }).toArray();
+            }
+            return Math.round(x/1000000);
+        }).filter(x -> x % 2 == 0).toArray();
         Collection<Integer> list =Arrays.stream(numbers).boxed().collect(Collectors.toList());
         IntSummaryStatistics stats = list.stream().mapToInt(Integer::intValue).summaryStatistics();
-        System.out.println(stats.getAverage());
+        System.out.println("Среднее арифметическое из:\n" + list + "\n" + "равно - " + stats.getAverage());
     }
 
 
